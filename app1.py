9c9c9821a95bfe086763aaf999f8e68240e9aa4b
Rubadarshini <85794429+ruba6@users.noreply.github.com>
2025-10-28 18:32:07 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -8,7 +8,7 @@ app=Flask(__name__,template_folder='templates')
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="Home")
+    return render_template('index.html', title="rubadarshini_portfolio_home")
 
 @app.route('/download_cv')
 def download_cv():
